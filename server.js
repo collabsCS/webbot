8e18380b87d8d7e74d596d716f813bbe5a5116ac
collabsCS <support@cs-collabs.com>
2025-05-10 01:21:32 -0300

    Actualización de archivos y configuración de servidor para despliegue en Railway

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// server.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la carpeta "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(__dirname));
 
 // Ruta principal (index.html)
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Iniciar servidor
